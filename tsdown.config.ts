97da354668119a089af660b31282ed4cecec6dd5
selemondev <selemondev19@gmail.com>
2025-11-22 13:01:14 -0700

    chore(tsdown): produce tree-shakeable output

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,16 +4,7 @@ export default defineConfig({
   entry: "src/index.ts",
   format: ["esm", "cjs"],
   dts: true,
+  treeshake: false,
   sourcemap: true,
-  minify: true,
-  exports: true,
-  clean: true,
-  external: [
-    "expo",
-    "expo-file-system",
-    "expo-image-picker",
-    "expo-constants",
-    "react-native",
-    "react",
-  ],
+  exports: true
 });
